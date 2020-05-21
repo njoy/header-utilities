a44f552e9150a56f1ca8c1f93774e346b6b13ba3
Nathan Gibson <ngibson@lanl.gov>
2020-05-21 15:01:13 -0600

    Added GIT_SHALLOW

--- cmake/header-utilities_dependencies.cmake
+++ cmake/header-utilities_dependencies.cmake
@@ -13,11 +13,13 @@ include( FetchContent )
 FetchContent_Declare( catch-adapter
     GIT_REPOSITORY  http://github.com/njoy/catch-adapter
     GIT_TAG         origin/build/fetchcontent
+    GIT_SHALLOW     TRUE
     )
 
 FetchContent_Declare( Log
     GIT_REPOSITORY  http://github.com/njoy/Log
     GIT_TAG         origin/build/fetchcontent
+    GIT_SHALLOW     TRUE
     )
 
 
